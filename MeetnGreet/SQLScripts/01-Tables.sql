fe5003aa02ad8495d64cb94836740b3f1647fe3c
Ittapopo <stianstian95gmail.com>
2021-10-20 14:13:52 +0200

    db migration works!

---
+++ MeetnGreet/SQLScripts/01-Tables.sql
@@ -0,0 +1,64 @@
+﻿CREATE TABLE dbo.Meeting(
+	MeetingId int IDENTITY(1,1) NOT NULL,
+	Title nvarchar(100) NOT NULL,
+	Content nvarchar(max) NOT NULL,
+	UserId nvarchar(150) NOT NULL,
+	UserName nvarchar(150) NOT NULL,
+	Created datetime2(7) NOT NULL,
+ CONSTRAINT PK_Meeting PRIMARY KEY CLUSTERED 
+(
+	MeetingId ASC
+)
+) 
+GO
+
+CREATE TABLE dbo.Guest(
+	GuestId int IDENTITY(1,1) NOT NULL,
+	MeetingId int NOT NULL,
+	Content nvarchar(max) NOT NULL,
+	UserId nvarchar(150) NOT NULL,
+	UserName nvarchar(150) NOT NULL,
+	Created datetime2(7) NOT NULL,
+ CONSTRAINT PK_Guest PRIMARY KEY CLUSTERED 
+(
+	GuestId ASC
+)
+) 
+GO
+ALTER TABLE dbo.Guest  WITH CHECK ADD  CONSTRAINT FK_Guest_Meeting FOREIGN KEY(MeetingId)
+REFERENCES dbo.Meeting (MeetingId)
+ON UPDATE CASCADE
+ON DELETE CASCADE
+GO
+ALTER TABLE dbo.Guest CHECK CONSTRAINT FK_Guest_Meeting
+GO
+
+SET IDENTITY_INSERT dbo.Meeting ON 
+GO
+INSERT INTO dbo.Meeting(MeetingId, Title, Content, UserId, UserName, Created)
+VALUES(1, 'Status update meeting1', 
+		'Quick breifing about the latest developmentQuick breifing about the latest development',
+		'1',
+		'bob.test@test.com',
+		'2019-05-18 14:32')
+
+INSERT INTO dbo.Meeting(MeetingId, Title, Content, UserId, UserName, Created)
+VALUES(2, 'Regarding the community potluck1', 
+		'We still need more preperationsWe still need more preperations',
+		'2',
+		'jane.test@test.com',
+		'2019-05-18 14:48')
+GO
+SET IDENTITY_INSERT dbo.Meeting OFF
+GO
+
+SET IDENTITY_INSERT dbo.Guest ON 
+GO
+INSERT INTO dbo.Guest(GuestId, MeetingId, Content, UserId, UserName, Created)
+VALUES(1, 1, 'So excited about this! I will be there!', '2', 'jane.test@test.com', '2019-05-18 14:40')
+
+INSERT INTO dbo.Guest(GuestId, MeetingId, Content, UserId, UserName, Created)
+VALUES(2, 1, 'I cant make it this time. So sorry', '3', 'fred.test@test.com', '2019-05-18 16:18')
+GO
+SET IDENTITY_INSERT dbo.Guest OFF 
+GO
---
+++ MeetnGreet/SQLScripts/02-Sprocs.sql
@@ -0,0 +1,260 @@
+﻿CREATE PROC dbo.Guest_Delete
+	(
+	@GuestId int
+)
+AS
+BEGIN
+	SET NOCOUNT ON
+
+	DELETE
+	FROM dbo.Guest
+	WHERE GuestID = @GuestId
+END
+GO
+
+CREATE PROC dbo.Guest_Get_ByMeetingId
+	(
+	@MeetingId int
+)
+AS
+BEGIN
+	SET NOCOUNT ON
+
+	SELECT GuestId, MeetingId, Content, Username, Created
+	FROM dbo.Guest
+	WHERE MeetingId = @MeetingId
+END
+GO
+
+CREATE PROC dbo.Guest_Post
+	(
+	@MeetingId int,
+	@Content nvarchar(max),
+	@UserId nvarchar(150),
+	@UserName nvarchar(150),
+	@Created datetime2
+)
+AS
+BEGIN
+	SET NOCOUNT ON
+
+	INSERT INTO dbo.Guest
+		(MeetingId, Content, UserId, UserName, Created)
+	SELECT @MeetingId, @Content, @UserId, @UserName, @Created
+
+	SELECT GuestId, Content, UserName, UserId, Created
+	FROM dbo.Guest
+	WHERE GuestId = SCOPE_IDENTITY()
+END
+GO
+
+CREATE PROC dbo.Guest_Put
+	(
+	@GuestId int,
+	@Content nvarchar(max)
+)
+AS
+BEGIN
+	SET NOCOUNT ON
+
+	UPDATE dbo.Guest
+	SET Content = @Content
+	WHERE GuestId = @GuestId
+
+	SELECT a.GuestId, a.MeetingId, a.Content, u.UserName, a.Created
+	FROM dbo.Guest a
+		LEFT JOIN AspNetUsers u ON a.UserId = u.Id
+	WHERE GuestId = @GuestId
+END
+GO
+
+
+CREATE PROC dbo.Meeting_AddForLoadTest
+AS
+BEGIN
+	DECLARE @i int = 1
+
+	WHILE @i < 10000
+	BEGIN
+		INSERT INTO dbo.Meeting
+			(Title, Content, UserId, UserName, Created)
+		VALUES('Meeting ' + CAST(@i AS nvarchar(5)), 'Content ' + CAST(@i AS nvarchar(5)), 'User1', 'User1', GETUTCDATE())
+		SET @i = @i + 1
+	END
+END
+GO
+
+CREATE PROC dbo.Meeting_Delete
+	(
+	@MeetingId int
+)
+AS
+BEGIN
+	SET NOCOUNT ON
+
+	DELETE
+	FROM dbo.Meeting
+	WHERE MeetingID = @MeetingId
+END
+GO
+
+CREATE PROC dbo.Meeting_Exists
+	(
+	@MeetingId int
+)
+AS
+BEGIN
+	SET NOCOUNT ON
+
+	SELECT CASE WHEN EXISTS (SELECT MeetingId
+		FROM dbo.Meeting
+		WHERE MeetingId = @MeetingId) 
+        THEN CAST (1 AS BIT) 
+        ELSE CAST (0 AS BIT) END AS Result
+
+END
+GO
+
+CREATE PROC dbo.Meeting_GetMany
+AS
+BEGIN
+	SET NOCOUNT ON
+
+	SELECT MeetingId, Title, Content, UserId, UserName, Created
+	FROM dbo.Meeting 
+END
+GO
+
+CREATE PROC dbo.Meeting_GetMany_BySearch
+	(
+	@Search nvarchar(100)
+)
+AS
+BEGIN
+	SET NOCOUNT ON
+
+		SELECT MeetingId, Title, Content, UserId, UserName, Created
+		FROM dbo.Meeting 
+		WHERE Title LIKE '%' + @Search + '%'
+
+	UNION
+
+		SELECT MeetingId, Title, Content, UserId, UserName, Created
+		FROM dbo.Meeting 
+		WHERE Content LIKE '%' + @Search + '%'
+END
+GO
+
+CREATE PROC dbo.Meeting_GetMany_BySearch_WithPaging
+	(
+	@Search nvarchar(100),
+	@PageNumber int,
+	@PageSize int
+)
+AS
+BEGIN
+	SELECT MeetingId, Title, Content, UserId, UserName, Created
+	FROM
+		(	SELECT MeetingId, Title, Content, UserId, UserName, Created
+			FROM dbo.Meeting 
+			WHERE Title LIKE '%' + @Search + '%'
+
+		UNION
+
+			SELECT MeetingId, Title, Content, UserId, UserName, Created
+			FROM dbo.Meeting 
+			WHERE Content LIKE '%' + @Search + '%') Sub
+	ORDER BY MeetingId
+	OFFSET @PageSize * (@PageNumber - 1) ROWS
+    FETCH NEXT @PageSize ROWS ONLY
+END
+GO
+
+CREATE PROC dbo.Meeting_GetMany_WithGuests
+AS
+BEGIN
+	SET NOCOUNT ON
+
+	SELECT q.MeetingId, q.Title, q.Content, q.UserName, q.Created,
+		a.MeetingId, a.GuestId, a.Content, a.Username, a.Created
+	FROM dbo.Meeting q
+		LEFT JOIN dbo.Guest a ON q.MeetingId = a.MeetingId
+END
+GO
+
+CREATE PROC dbo.Meeting_GetSingle
+	(
+	@MeetingId int
+)
+AS
+BEGIN
+	SET NOCOUNT ON
+
+	SELECT MeetingId, Title, Content, UserId, Username, Created
+	FROM dbo.Meeting 
+	WHERE MeetingId = @MeetingId
+END
+GO
+
+CREATE PROC dbo.Meeting_GetUnanswered
+AS
+BEGIN
+	SET NOCOUNT ON
+
+	SELECT MeetingId, Title, Content, UserId, UserName, Created
+	FROM dbo.Meeting q
+	WHERE NOT EXISTS (SELECT *
+	FROM dbo.Guest a
+	WHERE a.MeetingId = q.MeetingId)
+END
+GO
+
+CREATE PROC dbo.Meeting_Post
+	(
+	@Title nvarchar(100),
+	@Content nvarchar(max),
+	@UserId nvarchar(150),
+	@UserName nvarchar(150),
+	@Created datetime2
+)
+AS
+BEGIN
+	SET NOCOUNT ON
+
+	INSERT INTO dbo.Meeting
+		(Title, Content, UserId, UserName, Created)
+	VALUES(@Title, @Content, @UserId, @UserName, @Created)
+
+	SELECT SCOPE_IDENTITY() AS MeetingId
+END
+GO
+
+CREATE PROC dbo.Meeting_Put
+	(
+	@MeetingId int,
+	@Title nvarchar(100),
+	@Content nvarchar(max)
+)
+AS
+BEGIN
+	SET NOCOUNT ON
+
+	UPDATE dbo.Meeting
+	SET Title = @Title, Content = @Content
+	WHERE MeetingID = @MeetingId
+END
+GO
+
+CREATE PROC dbo.Guest_Get_ByGuestId
+	(
+	@GuestId int
+)
+AS
+BEGIN
+	SET NOCOUNT ON
+
+	SELECT GuestId, Content, Username, Created
+	FROM dbo.Guest 
+	WHERE GuestId = @GuestId
+END
+GO
