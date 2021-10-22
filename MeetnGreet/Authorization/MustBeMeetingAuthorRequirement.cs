31648d91505041c2c4a29fc0a46bec3f420f8496
Ittapopo <stianstian95gmail.com>
2021-10-22 15:05:24 +0200

    still an await problem

--- MeetnGreet/Authorization/MustBeMeetingAuthorRequirement.cs
+++ MeetnGreet/Authorization/MustBeMeetingAuthorRequirement.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 
 namespace MeetnGreet.Authorization
 {
@@ -10,7 +6,6 @@ namespace MeetnGreet.Authorization
     {
         public MustBeMeetingAuthorRequirement()
         {
-
         }
     }
 }
